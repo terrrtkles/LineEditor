70ac8d518ef43b3ddc6ff2473c7b56841d75bda8
Teresa Cheung <32111156+terrrtkles@users.noreply.github.com>
2018-09-13 23:03:26 -0400

    cmake

--- LineEditor.cpp
+++ LineEditor.cpp
@@ -1,8 +1,36 @@
+#include "LineEditorHeader.h"
 #include <iostream>
+#include <string>
 
-int main(){
-    std::cout << "hello World" << std::endl;
-    return 0;
+using namespace std;
+
+
+int menu(){
+    char command[40];
+    cin >> command;
+
+
+    if(command.substr(0,6) == "insert") {
+        if (command.substr(6, 9) == "end") {
+            cout << "insert end thing method" << endl;
+            return 1;
+        }
+        //space before the line number
+
+        if (isdigit(command[7])){
+            cout << "insert at line number method" << endl;
+            return 2;
+        }
+
+    }
+
 }
 
+int main(){
 
+    int choice = menu();
+
+    cout << choice << endl;
+
+    return 0;
+}
