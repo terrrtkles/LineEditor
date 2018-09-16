fa4fe9b9e4bbc8e85e977ab9a70e22730bb63123
Teresa Cheung <32111156+terrrtkles@users.noreply.github.com>
2018-09-16 17:55:42 -0400

    Update LineEditor.cpp

--- LineEditor.cpp
+++ LineEditor.cpp
@@ -1,12 +1,14 @@
 #include <iostream>
 #include <string>
 #include <algorithm>
+#include <cstdlib>
 
 using namespace std;
 
-
-int menu(){
-	string command;
+int menu() {
+    string command;
+    int line = 0;
+    string textEnd;
 
     while (true) {
         getline(cin, command);
@@ -14,23 +16,40 @@ int menu(){
         if (command.find("insert") != string::npos) {
             cout << "ok";
             if (command.find("end") != string::npos) {
+
+                //insertEnd();
                 cout << "insert end thing method" << endl;
-
                 return 1;
-            }
-
-            else if (any_of(command.begin(), command.end(), :: isdigit)) { //any_of uses algorithm lib
+            } else if (any_of(command.begin(), command.end(), ::isdigit)) { //any_of uses algorithm lib
+                //insertAtLine();
                 cout << "insert at line number method" << endl;
                 return 2;
             }
             else {
                 cout << "Invalid command, please try again." << endl;
-
             }
-
+        }
+        else if (command.find("delete") != string::npos) {
+            line = atoi(command);
+            //deleteAtIndex();
+            return 3;
+        }
+        else if (command.find("edit") != string::npos) {
+            //edit();
+        }
+        else if (command.find("print") != string::npos) {
+            //print();
+        }
+        else if (command.find("search") != string::npos) {
+            //search();
+        }
+        else if (command.find("quit") != string::npos) {
+            return 0;
+        }
+        else {
+            cout << "Invalid command, please try again." << endl;
         }
     }
-
 }
 
 int main(){
